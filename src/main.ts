37a08994846cbcc43481a3f6923d53af7a03bff0
Mathias Bresciani <mathias.bresciani2003@gmail.com>
2025-05-30 01:05:25 +0200

    Revert "erd"
    This reverts commit bbbdde0f550145aa24daa941d6d4c8139fd340ee.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser'
-import { AppComponent } from './app/app.component'
-import { provideRouter } from '@angular/router'
-import { APP_INITIALIZER, importProvidersFrom } from '@angular/core'
-import { AuthService } from './app/core/services/auth.service'
-import { provideHttpClient } from '@angular/common/http'
-import { routes } from './app/app.routes'
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-export function initApp(authService: AuthService) {
-  return () => authService.init()
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initApp,
-      deps: [AuthService],
-      multi: true
-    },
-  ]
-})
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
