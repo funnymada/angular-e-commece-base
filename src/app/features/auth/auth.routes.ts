b556f97341c56f9ded14995b9204e999751c2c9d
mada <alessandromadas@gmail.com>
2025-05-30 02:24:07 +0200

    login perfetto

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,15 @@
 import type { Routes } from "@angular/router"
 import { LoginComponent } from "./login/login.component"
-
+import { guestGuard } from "../../core/guards/guest.guards"
 export const AUTH_ROUTES: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "", redirectTo: "login", pathMatch: "full" },
+  {
+    path: "login",
+    component: LoginComponent,
+    canActivate: [guestGuard], 
+  },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  }
 ]
-
